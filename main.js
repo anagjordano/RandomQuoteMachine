2fa232fdd4efa4b147a22340cd7544c98286f75f
Ana <elso9000@hotmail.com>
2017-12-01 20:28:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// PATH TO API: https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en
+
+$("document").ready(function(){
+
+var quote;
+var author; 
+
+    function getNewQuote(){
+        $.ajax({
+            url: 'https://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType: 'jsonp',
+
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format: 'jsonp'
+            },
+
+            success: function(value){
+                quote = value.quoteText;
+                author = value.quoteAuthor;
+                $('#quote').html(quote);
+                $('#author').html('- ' + author);
+                
+                if(!value.quoteAuthor){
+                    $('#author').html('- Anonymous');                    
+                }
+            }
+        });
+    };
+    getNewQuote();
+
+    $('.get-quote').on('click', function(event){
+        event.preventDefault();
+        getNewQuote();
+    });
+
+    $('.share-quote').on('click', function(){
+        window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(quote + " — " + author));
+    })
+});
